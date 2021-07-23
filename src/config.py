4072e46b8c1540549a67aabeb5274fcd135a1775
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-07-23 12:42:51 +0800

    [add] MEDIA_URL

--- src/config.py
+++ src/config.py
@@ -13,3 +13,35 @@
 上櫃公司_欄位 = 0
 
 台灣股市資訊網_基本資料_網址 = "https://goodinfo.tw/StockInfo/BasicInfo.asp"
+
+MEDIA_URL = {
+    "Yahoo奇摩新聞": "https://tw.news.yahoo.com",
+    "Yahoo奇摩股市": "https://tw.stock.yahoo.com",
+    "ETtoday新聞雲": "https://www.ettoday.net",
+    "ETtoday財經雲": "https://finance.ettoday.net",
+    "自由時報電子報": "https://news.ltn.com.tw",
+    "自由財經": "https://ec.ltn.com.tw",
+    "中時新聞網": "https://www.chinatimes.com",
+    "聯合新聞網": "https://udn.com",
+    "台視新聞": "https://news.ttv.com.tw",
+    "民視新聞": "https://www.ftvnews.com.tw",
+    "華視新聞網": "https://news.cts.com.tw",
+    "新浪新聞": "https://news.sina.com.tw",
+    "蘋果日報": "https://tw.appledaily.com",
+    "經濟日報": "https://money.udn.com",
+    "工商時報": "https://ctee.com.tw",
+    "科技新報": "https://technews.tw",
+    "數位時代": "https://www.bnext.com.tw",
+    "鉅亨網": "https://m.cnyes.com",
+    "鉅亨新聞網": "https://news.cnyes.com",
+    "東森財經新聞": "https://fnc.ebc.net.tw",
+    "信傳媒": "https://www.cmmedia.com.tw",
+    "風傳媒": "https://www.storm.mg",
+    "鏡週刊": "https://www.mirrormedia.mg",
+    "中央社": "https://www.cna.com.tw",
+    "中國廣播公司": "https://www.bcc.com.tw",
+    "財訊": "https://www.wealth.com.tw",
+    "DigiTimes": "http://www.digitimes.com.tw",
+    "MoneyDJ理財網": "https://www.moneydj.com",
+    "Youtube": "https://www.youtube.com",  # Not supported
+}
