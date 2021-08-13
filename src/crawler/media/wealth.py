b8d3655e2f51967dc83c3a859163119b85c20b6d
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-08-13 11:40:53 +0800

    [add] wealth

---
+++ src/crawler/media/wealth.py
@@ -0,0 +1,54 @@
+# encoding=utf-8
+# Author: Yu-Lun Chiang
+# Description: Get news
+
+import logging
+from typing import Dict, List, Union
+
+from bs4 import BeautifulSoup
+
+from src.crawler.media.base import BaseMediaCrawler
+from src.utils.struct import NewsStruct
+
+logger = logging.getLogger(__name__)
+
+
+class WealthNewsCrawler(BaseMediaCrawler):
+    """Web Crawler for Wealth News"""
+
+    MEDIA_CANDIDATES = ["財訊"]
+    CONTENT_ATTR_PATH = None
+
+    def getInfo(self, link: str) -> NewsStruct:
+        return super().getInfo(link)
+
+    def _get_content(self, soup: BeautifulSoup) -> str:
+        content = soup.find("div", itemprop="articleBody").text
+        logger.debug(f"CONTENT:\n {content}")
+        return content
+
+    @staticmethod
+    def _get_title(script_info: Dict[str, str], soup: BeautifulSoup) -> str:
+        title = soup.find(property="og:title").get("content")
+        logger.debug(f"TITLE: {title}")
+        return title
+
+    @staticmethod
+    def _get_keywords(
+        script_info: Dict[str, str], soup: BeautifulSoup
+    ) -> Union[List[str], None]:
+        keywords = soup.find("meta", {"name": "keywords"}).get("content").split("、")
+        logger.debug(f"KEYWORDS: {keywords}")
+        return keywords
+
+    @staticmethod
+    def _get_category(script_info: Dict[str, str], soup: BeautifulSoup) -> str:
+        category = soup.find("meta", property="article:section").get("content")
+        logger.debug(f"CATEGORY: {category}")
+        return category
+
+    @staticmethod
+    def _get_datetime(script_info: Dict[str, str], soup: BeautifulSoup) -> str:
+        datetime = soup.find("meta", property="article:published_time").get("content")
+        logger.debug(f"DATETIME: {datetime}")
+        return datetime
