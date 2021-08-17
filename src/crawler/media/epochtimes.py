5eb50dea875718d33b365c5fc6fa957a42b35f03
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-08-17 16:32:25 +0800

    [add] epochtimes

---
+++ src/crawler/media/epochtimes.py
@@ -0,0 +1,30 @@
+# encoding=utf-8
+# Author: Yu-Lun Chiang
+# Description: Get news
+
+import logging
+
+from bs4 import BeautifulSoup
+
+from src.crawler.media.base import BaseMediaNewsCrawler
+from src.utils.struct import NewsStruct
+
+logger = logging.getLogger(__name__)
+
+
+class EpochTimesNewsCrawler(BaseMediaNewsCrawler):
+    """Web Crawler for EpochTimes News"""
+
+    MEDIA_CANDIDATES = ["大紀元"]
+
+    def getInfo(self, link: str) -> NewsStruct:
+        return super().getInfo(link)
+
+    def _get_content(
+        self,
+        soup: BeautifulSoup,
+    ) -> str:
+
+        content = soup.find("div", itemprop="articleBody").text
+        logger.debug(f"CONTENT:\n {content}")
+        return content
