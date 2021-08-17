fea23f6afcd4ca475a5566c1dbd385fe0755212e
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-08-17 16:36:50 +0800

    [add] setn

---
+++ src/crawler/media/setn.py
@@ -0,0 +1,30 @@
+# encoding=utf-8
+# Author: Yu-Lun Chiang
+# Description: Get news
+
+import logging
+
+from bs4 import BeautifulSoup
+
+from src.crawler.media.base import BaseMediaNewsCrawler
+from src.utils.struct import NewsStruct
+
+logger = logging.getLogger(__name__)
+
+
+class SETNNewsCrawler(BaseMediaNewsCrawler):
+    """Web Crawler for SETN News"""
+
+    MEDIA_CANDIDATES = ["三立新聞"]
+
+    def getInfo(self, link: str) -> NewsStruct:
+        return super().getInfo(link)
+
+    def _get_content(
+        self,
+        soup: BeautifulSoup,
+    ) -> str:
+
+        content = soup.find("article").text
+        logger.debug(f"CONTENT:\n {content}")
+        return content
